d8eefddef283c12e57329703fd0bab3e179eee84
sagazluiz <sagazluiz@hotmail.com>
2023-11-27 21:48:17 -0300

    Adding some code

---
+++ Capitulo 10 Classes e Objetos.py
@@ -0,0 +1,19 @@
+
+class Televisao:
+    def __init__(self):
+        self.ligada = False
+        self.canal = 2
+        self.tamanho = '20'
+        self.marca = 'Semp'
+
+tv_sala = Televisao()
+tv_sala.ligada = False
+tv_sala.canal = 4
+tv_sala.tamanho = '20'
+tv_sala.marca = 'Samsung'
+
+tv_quarto = Televisao()
+tv_quarto.ligada = False
+tv_quarto.canal = 5
+tv_quarto.tamanho = '24'
+tv_quarto.marca = 'LG'
