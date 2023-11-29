d0b388cc2b9ac992af35b14b999da0276eeeb1f4
sagazluiz <sagazluiz@hotmail.com>
2023-11-28 22:01:31 -0300

    Some evolution

--- Capitulo 10 Classes e Objetos.py
+++ Capitulo 10 Classes e Objetos.py
@@ -1,3 +1,9 @@
+##############################################################################
+# Author: Fabio Araujo
+# Date:
+# Objective: exercicios3\capitulo 10\exercicio-10-01.py
+##############################################################################
+
 
 class Televisao:
     def __init__(self):
@@ -5,6 +11,10 @@ class Televisao:
         self.canal = 2
         self.tamanho = '20'
         self.marca = 'Semp'
+    def muda_canal_para_baixo(self):
+        self.canal -= 1
+    def muda_canal_para_cima(self):
+        self.canal += 1
 
 tv_sala = Televisao()
 tv_sala.ligada = False
@@ -17,3 +27,12 @@ tv_quarto.ligada = False
 tv_quarto.canal = 5
 tv_quarto.tamanho = '24'
 tv_quarto.marca = 'LG'
+
+tv = Televisao()
+tv.muda_canal_para_cima()
+tv.muda_canal_para_cima()
+print(f"O canal da tv é: {tv.canal}")
+
+tv.muda_canal_para_baixo()
+print(f"O canal da tv é: {tv.canal}")
+
