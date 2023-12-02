04772a10c0bb50dbb7ed2b2544b293c42cd56bed
sagazluiz <sagazluiz@hotmail.com>
2023-12-01 21:11:12 -0300

    Update

---
+++ cap-10/exec-06.py
@@ -0,0 +1,45 @@
+##############################################################################
+# Author: Fabio Araujo
+# Date:5# Objective: exercicios3\capitulo 10\exercicio-10-06.py
+##############################################################################
+
+class Conta:
+    def __init__(self, clientes, número, saldo=0):
+        self.saldo = 0
+        self.clientes = clientes
+        self.número = número
+        self.operações = []
+        self.deposito(saldo)
+
+    def resumo(self):
+        print(f"CC N°{self.número} Saldo: {self.saldo:10.2f}")
+
+    def saque(self, valor):
+        if self.saldo >= valor:
+            self.saldo -= valor
+            self.operações.append(["SAQUE", valor])
+        else:
+            print("Saldo insuficiente!")
+
+    def deposito(self, valor):
+        self.saldo += valor
+        self.operações.append(["DEPÓSITO", valor])
+
+    def extrato(self):
+        print(f"Extrato CC N° {self.número}\n")
+        for o in self.operações:
+            print(f"{o[0]:10s} {o[1]:10.2f}")
+        print(f"\n    Saldo: {self.saldo:10.2f}\n")
+
+
+class ContaEspecial(Conta):
+    def __init__(self, clientes, número, saldo=0, limite=0):
+        Conta.__init__(self, clientes, número, saldo)
+        self.limite = limite
+
+    def saque(self, valor):
+        if self.saldo + self.limite >= valor:
+            self.saldo -= valor
+            self.operações.append(["SAQUE", valor])
+        else:
+            Conta.saque(self, valor)
